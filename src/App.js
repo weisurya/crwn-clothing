c8ea7a2671e010766f3a3220f04b50ab0c6ef058
Soen Surya Soenaryo <wei.surya@gmail.com>
2022-01-01 17:20:07 +0700

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, useParams } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div className="hats">
+    <h1>HATS PAGE</h1>
+  </div>  
+);
+
+const HatDetailPage = () => {
+  const params = useParams();
+  console.log(params);
+  return (
+    <div className="hat_detail">
+      <h1>HAT {params.hat_id} PAGE</h1>
+    </div>  
+  )
+};
+
 function App() {
   return (
-   <div>
-     <HomePage />
+   <div className="App">
+    <Routes>
+      <Route path='/' element={<HomePage />}/>
+      <Route path='/hats' element={<HatsPage/>}/>
+      <Route path='/hats/:hat_id' element={<HatDetailPage/>}/>
+      <Route 
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <h1>There's nothing here!</h1>
+          </main>
+        }
+      />
+    </Routes>
    </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-const HomePage = () => (
-    <div className='homepage'>
-        <Directory />
-    </div>
-)
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <Link to='/hats'> Hats</Link>
+            <Directory />
+        </div>
+    )
+}
 
 export default HomePage;
