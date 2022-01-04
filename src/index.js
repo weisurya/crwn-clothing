f67ff09feb341f415934bbe920671e1163988241
Soen Surya Soenaryo <wei.surya@gmail.com>
2022-01-04 13:00:53 +0700

    add redux-persist to store latest item in cart into local storage

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -13,7 +14,9 @@ const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   rootElement,
