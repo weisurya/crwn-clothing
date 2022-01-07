0fdd5fa6379cd08f5455893ed27e06cae3f99acd
Soen Surya Soenaryo <wei.surya@gmail.com>
2022-01-08 00:02:13 +0700

    updated our application App component, sign-in component, and sign-up component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -15,42 +16,37 @@ import Header from './components/header/header.component';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends React.Component {
-  unsubscribeFromAuth = null
-  
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-
+const App = ({
+  checkUserSession,
+  currentUser,
+}) => {
+  useEffect(() => {
     checkUserSession();
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route
-            exact
-            path='/signin'
-            render={() =>
-              this.props.currentUser ? (
-                <Redirect to='/' />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Switch>
-      </div>
-     );
-  }
+  }, [
+    checkUserSession,
+  ])
+  
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/checkout' component={CheckoutPage} />
+        <Route
+          exact
+          path='/signin'
+          render={() =>
+            currentUser ? (
+              <Redirect to='/' />
+            ) : (
+              <SignInAndSignUpPage />
+            )
+          }
+        />
+      </Switch>
+    </div>
+   );
 }
 
 const mapStateToProps = createStructuredSelector({
