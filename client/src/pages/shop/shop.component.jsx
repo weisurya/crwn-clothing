b745346583717311983615ce62baf76914af729e
Soen Surya Soenaryo <wei.surya@gmail.com>
2022-01-10 02:19:09 +0700

    Implement error boundary

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.action';
 
-import CollectionsOverviewContainer from '../../components/collection-overview/collection-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+import Spinner from '../../components/spinner/spinner.component';
+
+const CollectionsOverviewContainer = lazy(() => 
+    import('../../components/collection-overview/collection-overview.container')
+)
+
+const CollectionPageContainer = lazy(() =>
+    import('../collection/collection.container')
+)
 
 const ShopPage = ({ 
     fetchCollectionsStart, 
@@ -17,14 +24,16 @@ const ShopPage = ({
 
     return (
         <div className="shop-page">
-            <Route 
-                exact path={`${match.path}`}
-                component={CollectionsOverviewContainer}
-            />
-            <Route
-                path={`${match.path}/:collectionId`}
-                component={CollectionPageContainer}
-            />
+            <Suspense fallback={<Spinner />}>
+                <Route 
+                    exact path={`${match.path}`}
+                    component={CollectionsOverviewContainer}
+                />
+                <Route
+                    path={`${match.path}/:collectionId`}
+                    component={CollectionPageContainer}
+                />
+            </Suspense>
         </div>
     )
 }
